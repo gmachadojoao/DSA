b00c4730350a6e999c2188c417502ebe651cf540
João Pedro <jotadev4@gmail.com>
2025-02-18 09:50:17 -0300

    Update Q1.java
    Adição da Função "theEnd" para verificar se a palavra indicada o fim do codigo

--- TP's/TP 1/Q1.java
+++ TP's/TP 1/Q1.java
@@ -3,16 +3,22 @@ package Q1;
 import java.util.Scanner;
 
 public class Q1 {
+    public static boolean theEnd(String x) {
+        // Verifica se a string tem pelo menos 3 caracteres antes de acessar
+        if (x.length() == 3 && x.charAt(0) == 'F' && x.charAt(1) == 'I' && x.charAt(2) == 'M') {
+            return true;
+        }
+        return false;
+    }
 
     public static boolean ehPalin(String x) {
         int n = x.length();
-        boolean aux = true;
         for (int i = 0; i < n / 2; i++) {
             if (x.charAt(i) != x.charAt(n - 1 - i)) {
-                return aux = false;
+                return false;
             }
         }
-        return aux;
+        return true;
     }
 
     public static void main(String[] args) {
@@ -22,14 +28,13 @@ public class Q1 {
 
         while (status) {
             input = sc.nextLine();
-            if (input.equals("FIM")) {
+            if (theEnd(input)) {
                 status = false;
             } else if (ehPalin(input)) {
                 System.out.println("SIM");
             } else {
                 System.out.println("NAO");
             }
-
         }
 
         sc.close();
