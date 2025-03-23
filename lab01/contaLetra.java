2a66346ea1b27d286d1185d9e8981b56622bca02
João Pedro <jotadev4@gmail.com>
2025-03-23 16:04:05 -0300

    Update contaLetra.java

--- lab01/contaLetra.java
+++ lab01/contaLetra.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 
-public class contaLetra {
-    public static int contaLetra(String world) {
+public class ContaMaiuscula {
+
+    // Método para verificar se a palavra é "FIM"
+    public static boolean theEnd(String str) {
+        if (str.length() == 3 && str.charAt(0) == 'F' && str.charAt(1) == 'I' && str.charAt(2) == 'M') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static int contaLetra(String palavra) {
         int count = 0;
-        for (int i = 0; i < world.length(); i++) {
-            if (Character.isUpperCase(world.charAt(i))) {
+        for (int i = 0; i < palavra.length(); i++) {
+            if (Character.isUpperCase(palavra.charAt(i))) {
                 count++;
             }
         }
@@ -15,10 +25,14 @@ public class contaLetra {
         Scanner sc = new Scanner(System.in);
         String palavra = "";
 
-        while (palavra != "FIM") {
+        while (true) {
             palavra = sc.nextLine();
-            System.out.println(contaLetra(palavra));
 
+            if (theEnd(palavra)) {
+                break;
+            }
+
+            System.out.println(contaLetra(palavra));
         }
 
         sc.close();
